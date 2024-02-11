91fe38dcfeb60485dd6e4a51b86b19f45c63041b
unknown <carolineguzylak@gmail.com>
2024-02-10 16:37:59 -0800

    created add and remove admin buttons and popup modals

--- frontend/src/routes/dashboard/DashboardPage.jsx
+++ frontend/src/routes/dashboard/DashboardPage.jsx
@@ -4,6 +4,11 @@ import Typography from "@mui/material/Typography";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { Button } from "@mui/material";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import CloseIcon from "@mui/icons-material/Close";
 
 import NotificationTable from "../../components/NotificationTable";
 import CardCarousel from "../../components/CardCarousel";
@@ -11,6 +16,131 @@ import EventBar from "../../components/EventBar";
 import AdminTable from "../../components/AdminTable";
 
 import UserContext from "../../user/UserContext";
+
+// Used to style pop-up modals
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 700,
+  bgcolor: "background.paper",
+  borderRadius: 1,
+  //   border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  gap: "20px",
+};
+
+// pop-up for when superadmin clicks 'add admin'
+function AddAdminModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpenAddAdmin = () => setOpen(true);
+  const handleCloseAddAdmin = () => setOpen(false);
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpenAddAdmin} variant="outlined">
+        Add admins
+      </Button>
+
+      {/* Pop-up modal to add an admin */}
+      <Modal
+        open={open}
+        onClose={handleCloseAddAdmin}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          {/* Header */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography id="modal-modal-title" variant="h2">
+              Add administrator to program
+            </Typography>
+            <Button variant="text" endIcon={<CloseIcon />}>
+              Nevermind
+            </Button>
+          </Box>
+          <Typography id="modal-modal-description" variant="body1">
+            Enter a UC Irvine email address (ends in “@uci.edu”) to add a new
+            administrator to this learning support program:
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            sx={{ width: "100%" }}
+          />
+          <Button variant="contained" sx={{ width: "25%" }}>
+            Add new admin
+          </Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+// pop-up for when superadmin clicks 'remove admin'
+function RemoveAdminModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpenRemoveAdmin = () => setOpen(true);
+  const handleCloseRemoveAdmin = () => setOpen(false);
+
+  return (
+    <React.Fragment>
+      <Button onClick={handleOpenRemoveAdmin} variant="text" color="error">
+        Remove admins
+      </Button>
+
+      {/* Pop-up modal to remove an admin */}
+      <Modal
+        open={open}
+        onClose={handleCloseRemoveAdmin}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle}>
+          {/* Header */}
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography id="modal-modal-title" variant="h2">
+              Remove administrator from program
+            </Typography>
+            <Button variant="text" endIcon={<CloseIcon />}>
+              Nevermind
+            </Button>
+          </Box>
+          <Typography id="modal-modal-description" variant="body1">
+            Select an administrator to revoke their administrator permissions
+            from this learning support program:
+          </Typography>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            sx={{ width: "100%" }}
+          />
+          <Button variant="contained" color="error" sx={{ width: "25%" }}>
+            Remove admin
+          </Button>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
 
 const DashboardPage = () => {
   const userData = useContext(UserContext);
@@ -50,6 +180,7 @@ const DashboardPage = () => {
         </div>
       </div>
 
+      {/* Program admin table only shownt to admin or superadmin */}
       {(userData.role === "admin" || userData.role === "superadmin") && (
         <div>
           <div className="h2Container">
@@ -61,12 +192,23 @@ const DashboardPage = () => {
         </div>
       )}
 
-      <div>
-        <div className="h2Container">
-          <Typography variant="h2">Followed programs</Typography>
+      {/* Buttons to change admins only shown to superadmin */}
+      {userData.role === "superadmin" && (
+        <div>
+          <AddAdminModal />
+          <RemoveAdminModal />
         </div>
-        <CardCarousel cardType="followedPrograms" />
-      </div>
+      )}
+
+      {/* Only show followed programs to admins and super admins */}
+      {(userData.role === "admin" || userData.role === "student") && (
+        <div>
+          <div className="h2Container">
+            <Typography variant="h2">Followed programs</Typography>
+          </div>
+          <CardCarousel cardType="followedPrograms" />
+        </div>
+      )}
     </div>
   );
 };
