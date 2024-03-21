f3e2a00fc56a35da570d1781019e935dee3230ab
plwidjaj <plwidjaj@uci.edu>
2024-03-21 02:38:52 -0700

    Fix logo error

--- frontend/src/routes/landing/LandingPage.jsx
+++ frontend/src/routes/landing/LandingPage.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faNetworkWired, faBell, faComments } from '@fortawesome/free-solid-svg-icons';
 import { Typography, Button, Grid, Box, Container } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
-import Logo from '../logo.png';
+import Logo from '../Logo.png';
 import HeaderImg from '../Header.jpeg'; // Ensure this import path is correct
 import Student from '../students.jpg';
 
